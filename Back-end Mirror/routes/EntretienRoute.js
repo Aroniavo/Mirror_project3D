c47ccc8fba140e4611549978ba7f7b2bb4393353
Ruta Mickael <rutamickael619@gmail.com>
2025-10-03 08:10:13 +0300

    Update EntretienRoute.js

--- Back-end Mirror/routes/EntretienRoute.js
+++ Back-end Mirror/routes/EntretienRoute.js
@@ -2,7 +2,6 @@ const express = require("express")
 const router  = express.Router()
 const {getAllEntretien,createEntrt} = require("../controllers/EntretienController")
 const authenticateToken = require("../middleware/authentification")
-const { route } = require("./UserRoute")
 
 router.get("/allEntretien",authenticateToken,getAllEntretien)
 router.post("/createEntretien",authenticateToken,createEntrt)
